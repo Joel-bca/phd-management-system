fb31e0d37c9bfc52ae0d1c065d75467f676e7bcd
Joel <paul.joel@bcah.christuniversity.in>
2026-07-30 06:25:36 +0000

    added the collasible tsx

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,5 +1,7 @@
 import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible"
 
+import { cn } from "@/lib/utils"
+
 function Collapsible({ ...props }: CollapsiblePrimitive.Root.Props) {
   return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
 }
@@ -10,9 +12,22 @@ function CollapsibleTrigger({ ...props }: CollapsiblePrimitive.Trigger.Props) {
   )
 }
 
-function CollapsibleContent({ ...props }: CollapsiblePrimitive.Panel.Props) {
+function CollapsibleContent({
+  className,
+  style,
+  ...props
+}: CollapsiblePrimitive.Panel.Props) {
   return (
-    <CollapsiblePrimitive.Panel data-slot="collapsible-content" {...props} />
+    <CollapsiblePrimitive.Panel
+      data-slot="collapsible-content"
+      className={cn(
+        "overflow-hidden transition-[height] duration-300 ease-in-out",
+        "data-[starting-style]:h-0 data-[ending-style]:h-0",
+        className
+      )}
+      style={{ height: "var(--collapsible-panel-height)", ...style }}
+      {...props}
+    />
   )
 }
 
